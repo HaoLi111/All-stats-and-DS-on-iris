34cbd0c5c89ee16da70fe82932a472d93d967641
Hao <32727671+HaoLi111@users.noreply.github.com>
2020-02-17 11:38:47 +1300

    Add files via upload
    daddada

---
+++ tf.R
@@ -0,0 +1,12 @@
+
+install.packages('yaml')
+install.packages('jsonlite')
+devtools::install_github("rstudio/tensorflow")
+devtools::install_github("rstudio/keras")
+install.packages("tfestimators")
+library(tensorflow)
+install_tensorflow("conda",version='gpu')
+library(keras)
+?install_tensorflow
+install_tensorflow()
+?install_keras
